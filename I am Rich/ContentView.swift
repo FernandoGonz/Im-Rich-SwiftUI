3c3dbfd25715954cf55709089ec94fe22456e7c3
Fernando Gonzalez <ing.fernandogonzalez@outlook.com>
2021-09-02 12:39:53 -0500

    Ztack, VStack, Text and Image are implemented. Project Done

--- I am Rich/ContentView.swift
+++ I am Rich/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.title)
+                    //.font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding()
+                //asset name
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone XR"))
     }
 }
